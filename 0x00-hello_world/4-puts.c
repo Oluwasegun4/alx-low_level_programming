7bd27c9c703c804327d6027027ee70dcec62c636
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-08 04:02:32 -0700

    Use puts command to print s string in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'using puts to print a string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char MyStr[] = "Programming is like building a multilingual puzzle";
+
+	puts(MyStr);
+	return (0);
+
+}
