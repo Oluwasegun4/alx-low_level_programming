1d7c1b03401214f7713470a6ad51dc2b35fc678f
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-09 11:46:25 -0700

    Tell the type of the value of n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+* main - Entry point
+* Description: "his program prints the type of various values of n"
+*
+* Return: Always 0 (Success)
+*/
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
