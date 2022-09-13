b4c5073f4a0b220b395c9966d31fa2ebeabcce1a
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-13 10:32:12 -0700

    Func that prints the 9-times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* times_table - prints the time-table
+* Return: void
+*/
+
+void times_table(void)
+{
+	int n, m, z;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			z = n * m;
+			_putchar(z + '0');
+			if (m != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		_putchar('\n');
+		}
+	}
+}
