5cdaa6cdb40bdb20478e0f3f2dd9ff403bbbe425
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-13 09:30:37 -0700

    Create a func that computes absolut value of an int

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
 * _abs - computes the absolute value of an integer
-* @int: parameter integer
+* @x: parameter integer
 * Return: Always 0
 */
 
-int _abs(int)
+int _abs(int x)
 {
-	int x;
-
 	if (x >= 0)
 		_putchar(x);
 	else
