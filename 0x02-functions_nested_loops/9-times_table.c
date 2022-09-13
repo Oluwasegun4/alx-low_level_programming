142ccae8149998d0b74231a2e36edfabbefc6923
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-13 10:46:48 -0700

    Func that prints the 9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,19 @@ void times_table(void)
 		for (m = 0; m < 10; m++)
 		{
 			z = n * m;
-			_putchar((z / 10) + '0');
-			_putchar((z % 10) + '0');
+			if (z < 10)
+				_putchar(z + '0');
+			else
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
 			if (m != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
