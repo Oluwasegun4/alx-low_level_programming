e0bf964480a4b55324710ec555b14f96b30dd81e
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-13 14:01:45 -0700

    Func that print the sum of multiples of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_sum_natural - it prints the sum of natural numbers that are
+* multiples of 3 and 5
+*
+* return: void
+*/
+
+void print_sum_natural(void);
+{
+	int n, sum;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 || n % 5)
+			sum += n;
+	}
+	printf("%d\n", sum);
+}
