37015aeeb1e969753d31e1cba6121a724fe490b4
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-19 05:14:20 -0700

    Create a func that manipulate the val of a variable with its pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *reset_to_98 - takes a pointer as parameter and
+  *outputs 98 to the val it points to.
+  *@n: pointer parameter
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
