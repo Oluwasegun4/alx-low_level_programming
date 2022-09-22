39a2abb32743190226747175ae0964828bc2eeaa
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-22 01:46:17 -0700

    Create a func that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
- 	int i = 0; cp = 0;
-	while (s1[i] = '\0' && s2[i] = '\0' && cp == 0)
+	int i = 0, cp = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && cp == 0)
 	{
 		cp = s1[i] - s2[i];
 		i++;
