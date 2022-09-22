bf5ce9fef35b6aef26eead40eb12699a8d11e29f
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-22 01:28:50 -0700

    Create a func that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _strcmp - compares two strings
+  * @s1: first string
+  * @s2: second string
+  * Return: int
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cp;
+
+	cp = s1[0] - s2[0];
+	return (cp);
+}
