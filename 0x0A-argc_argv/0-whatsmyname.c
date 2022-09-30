238c7bf95d76e82c50823450eea5157cdbfc978f
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-30 16:09:40 -0700

    Create a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - prints out its program name
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0
+  */
+
+int main (int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	printf("\n");
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,8 @@
+## 0x0A. C - argc, argv
+
+## Tasks 
+This contains codes on understanding how argc and argv works
+
+
+##My Thoughts
+
