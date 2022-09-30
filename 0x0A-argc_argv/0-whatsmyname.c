0fc3a8a3d0c219deb4a8dccc34d07dce65d831f0
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-09-30 16:11:22 -0700

    Create a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
   * Return: Always 0
   */
 
-int main (int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s", argv[0]);
 	printf("\n");
