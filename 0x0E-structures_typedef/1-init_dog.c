990e1894b4b05b59d8c53029c29e79d6afbf07ad
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-10-10 13:54:21 -0700

    Create a func that reads a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+  * init_dog - create a function that defines struct dog
+  * @d: struct name
+  * @name: dog name
+  * @age: dog's age
+  * @owner: dog's owner
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
