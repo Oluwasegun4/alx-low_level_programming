d45e982f12c2a00689e0213619d961bfd70e1546
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-10-10 13:37:41 -0700

    Create a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - defines the parameters for the dog
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: defines the parameters of the dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
