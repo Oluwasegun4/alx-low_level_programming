ee525994978c64d677396986a0092347a69ceb94
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-10-10 14:44:48 -0700

    Create a new dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * struct dog - defines the parameters for the dog
  * @name: dog name
@@ -18,4 +19,5 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
