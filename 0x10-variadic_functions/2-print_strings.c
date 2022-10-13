b2a9d14d8c9f359845290d7b158d66263cc067b8
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-10-13 14:13:45 -0700

    Create a func that prints all strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: the separator constant
+ * @n: number of numbers to be printed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(ap, char *));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
