f15537bc81376280e73c0e064cdf260558de801d
Oluwasegun Omotosho <opomotosho@gmail.com>
2022-10-13 14:17:07 -0700

    Create a func that prints all strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,8 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(ap, char *));
-		if (separator != NULL && i != n - 1)
+			printf("%s", str);
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	va_end(ap);
